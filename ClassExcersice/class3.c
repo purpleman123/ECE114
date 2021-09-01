511c7989c756817ed8edc0f4949b586db45cc42a
purpleman123 <80710001+purpleman123@users.noreply.github.com>
2021-09-01 18:04:25 -0400

    class changes

---
+++ ClassExcersice/class3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    // need 2 variables
+    // pseudo code is explaining in words what you need the code to accomplish
+    int x, y;
+    printf("Enter Integer #1: ");
+    scanf("%d", &x); // need & address sign
+    printf("Enter Integer #2: ");
+    scanf("%d", &y);
+    printf("The sum of %d and %d is %d.", x, y, x+y);
+    return 0;
+}
+// LETS GO
