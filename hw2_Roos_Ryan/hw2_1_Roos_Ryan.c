d6385d35a790ffa67e4ead514bbc9d5afda50939
purpleman123 <80710001+purpleman123@users.noreply.github.com>
2021-09-12 10:22:51 -0400

    working on hw

--- hw2_Roos_Ryan/hw2_1_Roos_Ryan.c
+++ hw2_Roos_Ryan/hw2_1_Roos_Ryan.c
@@ -3,9 +3,15 @@
 // pi approximation program
 int main()
 {
-long double n;
-printf("How many iterations would you like to find of PI? ");
-scanf("%ld", &n);
-printf("here %ld", n);
+    long double f,x,n,z;
+    n=0.000000;
+    f=0;
+    x=1000000;
+    z=-1;
+while(n < x+1){
+    f= f+((z^n)/((2*n)+1));
+    ++n;
+}
+printf("%ld", f);
 return 0;
 }
