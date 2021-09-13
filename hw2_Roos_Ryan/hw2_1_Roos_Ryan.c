9d2f720e62ae292ccb1dc2341eb1f021801f86c0
purpleman123 <discory@icloud.com>
2021-09-13 16:19:01 -0400

    working on hw

--- hw2_Roos_Ryan/hw2_1_Roos_Ryan.c
+++ hw2_Roos_Ryan/hw2_1_Roos_Ryan.c
@@ -3,18 +3,18 @@
 // pi approximation program
 int main()
 {
-    long double n, f, r, i;
-    ;
+    long double n, f, r, i, sum;
     n=0;
+    r=0;
     printf("How many iterations would you like to find of PI? ");
     scanf("%0.20lf", i);
 
-    while (n<i)
+    while ( n < i )
     {
-        f=((pow(-1, n))/((2*n)+1))+f;
-        n++;
+        f=((pow(-1, n)) / ((2*n)+1));  
+        r=f+r;    
+        n++; 
     }
-
 r=f*4;
 printf("Final Approximation: %0.20lf", r);
 return 0;
