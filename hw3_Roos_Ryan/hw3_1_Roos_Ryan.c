8ad46b8a5cc0fc63860dbec0ce838314f8d6e694
purpleman123 <discory@icloud.com>
2021-09-21 16:47:32 -0400

    work

--- hw3_Roos_Ryan/hw3_1_Roos_Ryan.c
+++ hw3_Roos_Ryan/hw3_1_Roos_Ryan.c
@@ -2,12 +2,35 @@
 #include <math.h>
 
 int main(){
-    int fretnum;
-    double length;
+    int fretnum, fretcounter;
+    float length, nut, previousnut, frettofret;
+    char note;
+    fretcounter=0;
     printf("What's the scale length in mm? ");
-    scanf("%f", length);
+    scanf("%f", &length);
     printf("What's the number of frets? ");
-    scanf("%d\n\n", fretnum);
-    printf("note    fret    from nut(mm)    fret to fret(mm)\n");
+    scanf("%d", &fretnum);
+    printf("note     fret     from nut(mm)     fret to fret(mm)\n");
+    while (fretcounter!=fretnum+1)
+    {
+        nut=(length-(length*(pow(0.5,fretcounter/12.0))));
+        previousnut=(length-(length*(pow(0.5,(fretcounter-1)/12.0))));
+        frettofret=nut-previousnut;
+        printf("%-10d%-20.2f%-20.2f\n", fretcounter, nut, frettofret);
+
+        if (note==A)
+            note=A#;
+        else
+        {
+            if (note==A#)
+            note=B;
+            else
+            {
+                if (note==B)
+                note=C
+            }
+        }
+        fretcounter++;
+    }
 return 0;
 }
