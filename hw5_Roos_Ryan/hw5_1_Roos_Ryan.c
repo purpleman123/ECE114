4f6ebb750a9d01747fb58cb3f366133977206658
purpleman123 <discory@icloud.com>
2021-10-06 16:31:38 -0400

    hw progress

--- hw5_Roos_Ryan/hw5_1_Roos_Ryan.c
+++ hw5_Roos_Ryan/hw5_1_Roos_Ryan.c
@@ -6,12 +6,59 @@
 #define SIZE 8
 
 int main(){
-   if (int i=0; i <= SIZE) 
-    srand((unsigned)time(NULL)); // random number generation between 0-999
-        random = rand() % 99 + 1;
-} // loop until unique number is chosen, then add number to given spot in 1xi array until i = SIZE
+
+int randNums[SIZE];
+int number, random, outcome;
+int guessnum = 0, counter = 0, randomCheck = 0;
+srand((unsigned)time(NULL));
 
 
-//say miss if not proper input or if not one of 8 numbers, if one of 8 numbers say bingo and congrats
-//they have three guesses, if they miss all three then they lose. correct numbers are read out at end
-//clear input!
+while(counter < SIZE){
+
+    random = rand() % 99 + 1;
+    randNums[counter] = random;
+    for(int k = 0; k < SIZE; k++)
+    {
+        randomCheck = (random == randNums[k]);
+        if (randomCheck==1){
+            random = rand() % 99 + 1;
+            randNums[counter] = random;
+            continue;
+        }
+    }
+    counter++;
+}
+printf("The lottery machine has generated 8 numbers between 1 and 99.\nEnter your lucky numbers to see if they are one of them!\nYou can have three guesses!\n\n");
+
+while (guessnum < GUESS){
+    printf("Enter a number between 1 and 99: ");
+    scanf("%d", &number);
+
+    for(int j = 0; j < SIZE; j++)
+    {
+        outcome = (number == randNums[j]);
+        if (outcome == 1)
+        break;
+    }
+    if (outcome == 1)
+    {
+        printf("Bingo!\n\nCongratulations! You win Y^_^Y");
+        break;
+    }
+    if( (outcome == 0) && (guessnum == GUESS-1) ){
+        printf("\nSorry! You lost ~>_<~");
+        break;
+    }
+    printf("Miss!\n");
+    fflush(stdin);
+    guessnum++;
+}
+
+printf("\nThe 8 secret numbers are:\n");
+for (int i = 0; i < SIZE; i++)
+    printf("%d  ", randNums[i]); // need to order the output
+
+
+
+return 0;
+}
