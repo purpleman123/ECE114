465841b10fcd604fd1489c684de1ab601066e1ea
purpleman123 <discory@icloud.com>
2021-10-06 22:15:07 -0400

    finish #1 for hw 5, still needs comments

--- HomeWorkECE114/hw5_Roos_Ryan/hw5_1_Roos_Ryan.c
+++ HomeWorkECE114/hw5_Roos_Ryan/hw5_1_Roos_Ryan.c
@@ -1,67 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #define RANGE 99
 #define GUESS 3
 #define SIZE 8
 
 int main(){
 
-int randNums[SIZE];
-int sorted[SIZE] = {0};
-int number, random, outcome;
-int guessnum = 0, counter = 0, randomCheck = 1, b = 0;
-srand((unsigned)time(NULL));
+    int randNums[SIZE] = {0};
+    int sorted = 0;
+    int number, random, outcome, fillx;
+    int guessnum = 0, randomCheck = 1, duplicate = 0, counter = 0, counter2 = 0;
+    srand((unsigned)time(NULL));
 
-do{
-    while(counter < SIZE){
+        do{
+            duplicate = 0;
+            while(counter < SIZE){
+                random = rand() % 99 + 1;
+                randNums[counter] = random;
+                counter++;
+            }
+            for (int i = 0; i < SIZE; i++){
+                for (int k = i+1; k < SIZE; k++){
+                    if(randNums[i] == randNums[k]){
+                    duplicate = 1;  
+                    }
+                }
+            }
+            counter = 0;
+            printf("%d\n", duplicate);
 
-        random = rand() % 99 + 1;
-        randNums[counter] = random;
-        counter++;
-    }
-    for (int i = 0; i < SIZE; i++){
-        for (int k = 0; k < SIZE; i++){
-            randomCheck = (randNums[i]==randNums[k]); // need to fix this part
-            if (randomCheck == 1){
+        } while (duplicate == 1);
+    printf("The lottery machine has generated 8 numbers between 1 and 99.\nEnter your lucky numbers to see if they are one of them!\nYou can have three guesses!\n\n");
+
+    while (guessnum < GUESS){
+        printf("Enter a number between 1 and 99: ");
+        scanf("%d", &number);
+
+        for(int j = 0; j < SIZE; j++){
+            outcome = (number == randNums[j]);
+            if (outcome == 1)
                 break;
+        }
+        if (outcome == 1){
+            printf("Bingo!\n\nCongratulations! You win Y^_^Y");
+            break;
+        }
+        if( (outcome == 0) && (guessnum == GUESS-1) ){
+            printf("\nSorry! You lost ~>_<~");
+            break;
+        }
+        printf("Miss!\n");
+        fflush(stdin);
+        guessnum++;
+    }
+
+    printf("\nThe 8 secret numbers are:\n");
+
+    while (counter2 < SIZE){
+        for (int x = 0; x<SIZE; x++){
+            if(abs(randNums[x]-100) > sorted){
+                sorted = abs(randNums[x]-100);
+                fillx = x;
+            }
+            else{
+                continue;
             }
         }
-        if (randomCheck == 1){
-            break;
-        }
+        printf("%d  ", randNums[fillx]);
+        randNums[fillx]=100;
+        sorted = 0;
+        counter2++;
     }
-} while(randomCheck == 1);
-
-printf("The lottery machine has generated 8 numbers between 1 and 99.\nEnter your lucky numbers to see if they are one of them!\nYou can have three guesses!\n\n");
-
-while (guessnum < GUESS){
-    printf("Enter a number between 1 and 99: ");
-    scanf("%d", &number);
-
-    for(int j = 0; j < SIZE; j++){
-        outcome = (number == randNums[j]);
-        if (outcome == 1)
-            break;
-    }
-    if (outcome == 1){
-        printf("Bingo!\n\nCongratulations! You win Y^_^Y");
-        break;
-    }
-    if( (outcome == 0) && (guessnum == GUESS-1) ){
-        printf("\nSorry! You lost ~>_<~");
-        break;
-    }
-    printf("Miss!\n");
-    fflush(stdin);
-    guessnum++;
-}
-
-printf("\nThe 8 secret numbers are:\n");
-  for (int i = 0; i < SIZE; i++)
-    printf("%d  ", randNums[i]); // need to order the output 
-
-
-
 return 0;
 }
