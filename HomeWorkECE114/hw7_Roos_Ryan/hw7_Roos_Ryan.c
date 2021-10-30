40c50c48dd7f93dadfd4e51766c29b18bffd3eef
purpleman123 <discory@icloud.com>
2021-10-30 12:31:54 -0400

    yaya

--- HomeWorkECE114/hw7_Roos_Ryan/hw7_Roos_Ryan.c
+++ HomeWorkECE114/hw7_Roos_Ryan/hw7_Roos_Ryan.c
@@ -3,13 +3,15 @@
 #include <time.h>
 #include <string.h>
 
+// This program will take a txt file full of students and IDs and output them in a random order
+
 typedef struct STUDENT{
     int IDnum;
     char first[50];
     char last[50];
 }STUDENT;
 
-void shuffle(STUDENT * const ptd, int studentnum);
+void shuffle(STUDENT * const ptd, int studentnum); 
 
 int main(int argc, char * argv[]){
     FILE * fp;
@@ -19,8 +21,8 @@ int main(int argc, char * argv[]){
     STUDENT * ptd; 
     
 
-    if(argc == 1 || argc > 2){
-        printf("Random Order Generator!\n");
+    if(argc == 1 || argc > 2){                   // if statments for different command line inputs
+        printf("Random Order Generator!\n");     // if neither of these are met then program will operate normally
         printf("Usage: %s filename\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -31,22 +33,22 @@ int main(int argc, char * argv[]){
     }
       
     else{
-        //malloc and free to generate array of STUDENT structs
-        ptd = (STUDENT * ) malloc(100 * sizeof(STUDENT));
+        //malloc to generate array of STUDENT structs
+        ptd = (STUDENT * ) malloc(100 * sizeof(STUDENT)); // dynamic allocation for STUDENT array
 
         fp = fopen(argv[1], "r");
-        while (fgets(line, sizeof(line), fp) != NULL)
-        {
+        while (fgets(line, sizeof(line), fp) != NULL) // counts how many student structs are needed
+        { 
             studentnum++;
         }
         ptd[studentnum];
         //pull info from txt file 
         rewind(fp);
         while( i < studentnum ){
-            fscanf(fp, " %d %s %s ", &ptd[i].IDnum, ptd[i].last, ptd[i].first);
+            fscanf(fp, " %d %s %s ", &ptd[i].IDnum, ptd[i].last, ptd[i].first); // each student is pulled and put into the array
             i++;
         }
-        //output in table format in random order - loop spaces given the name length
+        //output in table format in random order
         printf("    Full Name                              ID\n");
         shuffle(ptd, studentnum);
 
@@ -57,15 +59,18 @@ int main(int argc, char * argv[]){
 
 
 void shuffle(STUDENT * const ptd, int studentnum){
+/* this program works by randomly out putting different 
+spots in the array not by changing the actual contents of
+the array */
 
 int randCheck, random, spacing;
 int randarray[studentnum];
-        for (int k = 0; k < studentnum; k++){
+        for (int k = 0; k < studentnum; k++){ // array for checking if that student has been picked already
             randarray[k] = 100;
         }
 srand((unsigned)time(NULL));
 
-for (int k = 0; k < studentnum; k++){
+for (int k = 0; k < studentnum; k++){ // this will generate a number out of the given number of students and then check that they havent been used already
     do{
         randCheck = 0;
         random = rand() % studentnum;
@@ -76,9 +81,9 @@ for (int k = 0; k < studentnum; k++){
         }
                 
         }while(randCheck == 1);
-        printf("%s %s", ptd[random].first, ptd[random].last);
+        printf("%s %s", ptd[random].first, ptd[random].last); // once the random number is checked that players information is printed
         spacing = strlen(ptd[random].first) + strlen(ptd[random].last) + 1;
-        for (int i = 0; i < 40 - spacing; i++){
+        for (int i = 0; i < 40 - spacing; i++){ // this is here to make sure the ID numbers line up no matter the name of the player
             printf(" ");
         }    
         printf("%06d\n", ptd[random].IDnum);
