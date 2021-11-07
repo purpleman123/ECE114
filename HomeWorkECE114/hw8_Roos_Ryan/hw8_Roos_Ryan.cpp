b50304a6c6baff4203ca665d7cebb0cb08f05a35
purpleman123 <discory@icloud.com>
2021-11-07 01:26:55 -0400

    working on hw

--- HomeWorkECE114/hw8_Roos_Ryan/hw8_Roos_Ryan.cpp
+++ HomeWorkECE114/hw8_Roos_Ryan/hw8_Roos_Ryan.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string.h>
 
-//need to make struct for a player
+//change name[] to string and all assocated things
+// add loop to make name spacing right
+// add loop to make age spacing right
+// make names output youngest to oldest
+// LOOK UP NEW C++ FORMATTING MIGHT MAKE IT EASIER
+
+struct PLAYER{
+    char name[30];
+    int age;
+};
 
 int main(int argc, char const *argv[]){
     using namespace std;
@@ -8,10 +18,49 @@ int main(int argc, char const *argv[]){
     int counter = 0;
     cout << "Enter number of players (max. 100): ";
     cin >> playernum;
-    //while(cin.get() != '\n'){continue;}
+    cin.clear();
+    while(cin.get() != '\n')
+        continue;
     while (playernum < 1 || playernum > 100){
         cout << "Wrong input! Enter again: ";
         cin >> playernum;
-       // while(cin.get() != '\n'){continue;}
+        cin.clear();
+        while(cin.get() != '\n')
+            continue;
     }
+
+    struct PLAYER players[playernum];
+
+    while (counter < playernum){
+        cout << "=== Player #" << counter+1 << " ===\n";
+        cout << "Name: ";
+        cin >> players[counter].name;
+        while(cin.get() != '\n')
+            continue;
+        cout << "Age: ";
+        cin >> players[counter].age;
+        cin.clear();
+        while(cin.get() != '\n')
+            continue;
+        while (players[counter].age < 1){
+            cout << "Wrong input! Enter again: ";
+            cin >> players[counter].age;
+            cin.clear();
+            while(cin.get() != '\n')
+                continue;
+    }
+        counter++;
+    }
+    //loop to print out all names
+    cout << "\n=======================================\n";
+    cout << "| Name                          | Age |\n";
+    cout << "=======================================\n";
+    for (int i = 0; i < playernum; i++){
+        cout << "| " << players[i].name;
+        cout << "| " << players[i].age << "  |\n";
+    }
+    
+    cout << "=======================================";
+
+return 0;
 }
