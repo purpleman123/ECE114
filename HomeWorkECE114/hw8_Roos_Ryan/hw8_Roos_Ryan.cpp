f4c050d74e722cd9d6db3d7a914e635e8ca5d341
purpleman123 <discory@icloud.com>
2021-11-07 17:25:10 -0500

    finished hw

--- HomeWorkECE114/hw8_Roos_Ryan/hw8_Roos_Ryan.cpp
+++ HomeWorkECE114/hw8_Roos_Ryan/hw8_Roos_Ryan.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <string.h>
+#include <iomanip>
 
-//change name[] to string and all assocated things
-// add loop to make name spacing right
-// add loop to make age spacing right
-// make names output youngest to oldest
-// LOOK UP NEW C++ FORMATTING MIGHT MAKE IT EASIER
+//Program function: ask user how many players between 1 and 100, then input name and age for each player, then finally print out players youngest to oldest in a table
 
+//player struct created here
 struct PLAYER{
-    char name[30];
+    std::string name;
     int age;
 };
-
 int main(int argc, char const *argv[]){
     using namespace std;
-    int playernum;
+    int playernum, i;
     int counter = 0;
+    int counter2 = 0;
+    int sorted = 0;
+    // user inputs number of players here and the input is validated
     cout << "Enter number of players (max. 100): ";
     cin >> playernum;
     cin.clear();
@@ -30,17 +29,15 @@ int main(int argc, char const *argv[]){
     }
 
     struct PLAYER players[playernum];
-
+    // this loop will loop for the desired number of players while allowing the user to input a name and age into each player struct in the array
     while (counter < playernum){
         cout << "=== Player #" << counter+1 << " ===\n";
         cout << "Name: ";
-        cin >> players[counter].name;
-        while(cin.get() != '\n')
-            continue;
+        getline(cin, players[counter].name);
         cout << "Age: ";
         cin >> players[counter].age;
         cin.clear();
-        while(cin.get() != '\n')
+        while(cin.get() != '\n') // age validated here
             continue;
         while (players[counter].age < 1){
             cout << "Wrong input! Enter again: ";
@@ -48,19 +45,32 @@ int main(int argc, char const *argv[]){
             cin.clear();
             while(cin.get() != '\n')
                 continue;
-    }
+        }
         counter++;
     }
     //loop to print out all names
     cout << "\n=======================================\n";
     cout << "| Name                          | Age |\n";
     cout << "=======================================\n";
-    for (int i = 0; i < playernum; i++){
-        cout << "| " << players[i].name;
-        cout << "| " << players[i].age << "  |\n";
+    //sorting loop
+    while(counter2 < playernum){
+        for (int x = 0; x< playernum; x++){
+            if(abs(players[x].age-100) > sorted){
+                sorted = abs(players[x].age-100);
+                i = x;
+            }
+            else{
+                continue;
+            }
+        }
+        cout << "| "; 
+        cout << left << setw(30) << setfill(' ') << players[i].name; // using c++ formatting fuctions to output properly no matter age or name length
+        cout << "| ";
+        cout << left << setw(3) << setfill(' ') << players[i].age << " |\n";
+        sorted = 0;
+        players[i].age = 100;
+        counter2++;
     }
-    
     cout << "=======================================";
-
 return 0;
 }
