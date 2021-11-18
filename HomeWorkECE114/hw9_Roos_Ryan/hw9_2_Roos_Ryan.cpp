4dd13221bfcb6bf2bf4e50bc8c194248ffc5ca36
purpleman123 <discory@icloud.com>
2021-11-18 12:15:21 -0500

    updates to hw

--- HomeWorkECE114/hw9_Roos_Ryan/hw9_2_Roos_Ryan.cpp
+++ HomeWorkECE114/hw9_Roos_Ryan/hw9_2_Roos_Ryan.cpp
@@ -4,23 +4,17 @@
 #include <string>
 #include <vector>
 
-/*
- * Define a struct called PERSON:
- * Holds a person's information: name, address, and age.
- */
+// Define a struc called PERSON:
 struct PERSON{
     std::string name;
     std::string address;
     int age;
-}
+};
 
 /* Function declarations here*/
-/*
-createPerson(): It takes the name, address, and age as arguments and returns a PERSON struct.
-addPerson(): It takes a reference to the people vector and a PERSON struct. It does not return anything. Use this function to add a person to the vector. 
-printPeople(): It takes a reference to the people vector and a PERSON struct. It does not return anything. Use this function to print the entire list.
-*/
-
+PERSON createPerson(std::string name = "Mickey Mouse", std::string address = "Disney World", int age = 94);
+void addPerson(std::vector<PERSON> inputVector, PERSON P);
+void printPeople(std::vector<PERSON> outputVector);
 
 int main() {
     // A list of people
@@ -31,25 +25,46 @@ int main() {
 
     p = createPerson("Minnie Mouse", "Disneyland");     // passing only one argument
     addPerson(people, p);
-
+people.push_back(p);
     p = createPerson("Donald Duck", "Disneyland", 90);    // passing three arguments
     addPerson(people, p);
-
+people.push_back(p);
     p = createPerson("Cash Close", "Off Campus", 21); // passing three arguments
     addPerson(people, p);
-
+people.push_back(p);
     p = createPerson();    // pass no arguments (use default arguments)
     addPerson(people, p);  // append the PERSON struct just created to the vector
-
+people.push_back(p);
     p = createPerson("Eric Cartman", "South Park", 24);  // passing three arguments
     addPerson(people, p);
-
+people.push_back(p);
     p = createPerson("Homer Simpsons", "Springfield", 32); // passing three arguments
     addPerson(people, p);
-
+people.push_back(p);
     printPeople(people);   // print all the people
 
     return 0;
 }
 
 // Add function definitions here
+PERSON createPerson(std::string name, std::string address, int age){
+    PERSON person;
+    person.name = name;
+    person.address = address;
+    person.age = age;
+return person;
+}
+
+void addPerson(std::vector<PERSON> inputVector, PERSON P){
+    inputVector.push_back(P);
+}
+
+void printPeople(std::vector<PERSON> outputVector){
+
+   std::cout << "List of People:\n";
+   for (int i = 0; i < 6; i++){
+        std::cout << outputVector[i].name << "\n";
+        std::cout << "   Address: " << outputVector[i].address << "\n";
+        std::cout << "   Age: " << outputVector[i].age << "\n";
+    }
+}
