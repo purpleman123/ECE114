f9de8b74e0b0bdadff00516e6bb9bad9c39aac82
purpleman123 <80710001+purpleman123@users.noreply.github.com>
2021-11-25 10:01:29 -0500

    start of game project folder

---
+++ Game_Project_Roos_Ryan/game_roos_ryan.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout<< "***** WELCOME TO AN ECE 114 GAME *****";
+    // game loop begins here 
+    // menu
+    // enter name and begin rulle explination 
+    // if I have extra time maybe make it same number of turns and player name to a text doc as a history of players
+return 0;
+}
